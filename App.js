da707e222974e527e2354aca4587d74024b01009
Muhammad Tahir Ali <decentchamuli-cs@hotmail.com>
2022-11-17 14:53:37 +0500

    .

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button, ToastAndroid } from 'react-native';
 
 export default function App() {
+
+  const [email, setEmail] = useState('')
+  
+  const handleChange = (text) => {
+    setEmail(text)
+  }
+
+  const handlePress = () => {
+    ToastAndroid.showWithGravityAndOffset(
+      email,
+      ToastAndroid.LONG,
+      ToastAndroid.BOTTOM,
+      25,
+      50
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text onPress={handlePress} style={styles.mainHeading}>Login Now</Text>
+      <View style={styles.InputView}>
+        <TextInput
+          style={styles.inputField}
+          placeholder="Enter Email"
+          onChangeText={handleChange}
+        />
+        <Button title='Send' onPress={handlePress}/>
+      </View>
+      <View>
+        {email && <Text>{email}</Text>}
+      </View>
     </View>
   );
 }
@@ -13,8 +40,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 50,
+    alignItems: 'center',
+    justifyItems: 'center',
+    borderWidth: 2,
+    borderColor: '#ccc',
+    backgroundColor: '#b6f0ff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  mainHeading: {
+    fontSize: 25,
+    marginBottom: 15,
+    flex: 1
+  },
+  InputView: {
+    flex: 4,
+    width: '70%',
+  },
+  inputField: {
+    borderWidth: 3,
+    borderColor: 'gray',
+    borderRadius: 5,
+    color: '#000',
+    paddingStart: 10,
   },
 });
